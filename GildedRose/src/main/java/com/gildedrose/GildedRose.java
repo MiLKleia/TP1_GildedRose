0c0ef7e11ead476679892baa90c74e862e93a42b
MiLKleia <ines.meyre@gmail.com>
2023-10-06 15:14:31 +0200

    new methode updateItem works

--- GildedRose/src/main/java/com/gildedrose/GildedRose.java
+++ GildedRose/src/main/java/com/gildedrose/GildedRose.java
@@ -8,7 +8,16 @@ class GildedRose {
   }
 
   public void updateQuality() {
+
+    
     for (int i = 0; i < items.length; i++) {
+
+
+      if(true){
+        items[i].updateItem();
+
+    }else{
+
       if (!items[i].name.equals("Aged Brie")
           && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
         if (items[i].quality > 0) {
@@ -57,6 +66,6 @@ class GildedRose {
           }
         }
       }
-    }
+    }}
   }
 }
--- GildedRose/src/main/java/com/gildedrose/Item.java
+++ GildedRose/src/main/java/com/gildedrose/Item.java
@@ -8,6 +8,56 @@ public class Item {
 
     public int quality;
 
+    public void updateItem(){
+        
+
+        switch(name){
+
+                default :
+                    quality +=(-1);
+                    if (sellIn == 0){
+                        quality +=(-1);
+                    } 
+                    break;
+
+                
+
+                case "Backstage passes to a TAFKAL80ETC concert":
+                        quality ++;
+                    if (sellIn == 0){
+                        quality = 0;
+                    } else if (sellIn < 6) {
+                        quality +=2;
+                    } else if (sellIn < 11) {
+                        quality ++;
+                        }
+                    break;    
+    
+                case "Sulfuras, Hand of Ragnaros":
+                    break; 
+    
+                case "Aged Brie":
+                    quality ++;
+                    if (sellIn == 0){
+                        quality ++; 
+                    }
+                    break;
+                }
+
+                
+
+        sellIn = Math.max(0, (sellIn - 1));
+        quality = Math.max(0,Math.min(50, (quality)));
+    
+    }
+
+
+
+
+
+
+
+
     public Item(String name, int sellIn, int quality) {
         this.name = name;
         this.sellIn = sellIn;
